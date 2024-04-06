e1ef3c9f5777621063c0993e2d03f167c6950764
edsantiago <edsantiago@correo.url.edu.gt>
2024-04-05 20:10:37 -0600

    refactor 01

--- Program.cs
+++ Program.cs
@@ -1,57 +1,121 @@
-﻿namespace Semana10{
-    class Program{
+namespace Semana10{
 
+    class Program1{
         public static void Main(){
-            int abonarCuenta = 0;
-            int periodoCapitalizacion = 0;
-            int contadorAbono = 0;
+            bool menu = true;
+            while(menu){
+            menuApp(menu);
+            }
+        }
+
+        public static void menuApp(bool menu){
+            bool solicitarDatos = false;
             double saldoCuenta = 0;
-            List<String> Logs = new List<string>();
+            string titulo = "Sistema Bancario";
             string[] datos = new string[6];
             string[] opciones = { "Ver informacion de la cuenta", "Compra de producto Financiero", "Venta de producto Financiero", "Abono a cuenta", "Simulacion paso del tiempo", "Salir" };
-            string titulo = "Sistema bancario";
-            bool menu = true;
-            bool solicitarDatos = false;
-            while (menu){
-                Console.SetCursorPosition((Console.WindowWidth-titulo.Length)/2,Console.CursorTop);
-                Console.Clear();
-                Console.WriteLine(titulo);
-                if (!solicitarDatos){
-                    datos = TomarDatos();
+
+            Console.Clear();
+/*             Console.SetCursorPosition((Console.WindowHeight - titulo.Length) / 2, Console.CursorTop); */
+            Console.WriteLine(titulo);
+            if(!solicitarDatos){
+                datos = tomaDatos();
+                solicitarDatos = true;
+                saldoCuenta = float.Parse(datos[5]);
+            }
+            opcionesPrincipales(opciones,datos,menu, saldoCuenta);
+
+        }
+
+        public static string[] tomaDatos(){
+            string[] datos = new string[6];
+            Console.WriteLine("1. Cuenta monetaria Quetzales.\n2.Cuenta monetaria Dolares\n3.Cuenta de ahorro en Quetzales.\n4.Cuenta de ahorro en Dolares.");
+            Console.Write("Seleccione su tipo de cuenta indicando la opcion acorde");
+            int tipoCuenta = int.Parse(Console.ReadLine() ?? string.Empty );
+            datos[0] = switchCase1(tipoCuenta);
+            
+            Console.WriteLine("A continuacion ingrese los datos solicitados");
+            
+            Console.Write("Ingrese su nombre: ");
+            datos[1] = Console.ReadLine() ?? string.Empty;
+            do{
+                Console.Write("Ingrese su numero de DPI (Campo estrico de 5 caracteres)");
+                datos[2] = Console.ReadLine() ?? string.Empty;
+            } while(datos[2].Length < 5 || datos[2].Length > 5);
+            
+            Console.Write("Ingrese su direccion de residencia: ");
+            datos[3] = Console.ReadLine() ?? string.Empty;
+            
+            Console.WriteLine("Ingrese su numero de telefono: ");
+            datos[4] = Console.ReadLine() ?? string.Empty;
+
+            datos[5] = "2500";
+            Console.WriteLine($"Su saldo inicial sera de {datos[5]}");
+            return datos;
+        }
+
+        public static string switchCase1( int tipoCuenta){
+            string retorno;
+            switch(tipoCuenta){
+                case 1:
+                    retorno = "Cuenta Monetaria Quetzales";
+                    break;
+                    
+                case 2:
+                    retorno = "Cuenta Monetaria Dolares";
+                    break;
+                    
+                case 3:
+                    retorno = "Cuenta de Ahorro Quetzales";
+                    break;
+                    
+                case 4:
+                    retorno = "Cuenta de ahorro Dolares";
+                    break;
+
+                default:
                     Console.Clear();
-                    solicitarDatos = true;
-                    saldoCuenta = float.Parse(datos[5]);
-                }
-                for (int i = 0; i < opciones.Length; i++){
-                    Console.WriteLine($"{i + 1}.{opciones[i]}");
-                }
-                Console.Write("Seleccione la accion que desea ejecutar indicando el numero de opcion: ");
-                string? menu_opciones = Console.ReadLine();
-                datos[5] = saldoCuenta.ToString("F2");
-                switch (menu_opciones){
-                    case "1":
-                        Console.Clear();
-                        Console.WriteLine($"Tipo de cuenta: {datos[0]}\nNombre cuentaHabiente: {datos[1]}\nNo. de DPI: {datos[2]}\nDireccion: {datos[3]}\nNo. de Telefono: {datos[4]}\nSaldo de cuenta: ${datos[5]}");
-                        Console.WriteLine("\n\nLogs: ");
-                        for (int i = 0; i < Logs.Count; i++){
-                            Console.WriteLine(Logs[i]);
-                        }
-                        Console.ReadKey();
-                        break;
+                    Console.WriteLine("Opcion invalida");
+                    Console.ReadKey();
+                    retorno = "";
+                    break;
+            }
 
-                    case "2":
-                        Console.Clear();
-                        saldoCuenta = saldoCuenta - (saldoCuenta * 0.1);
-                        Logs.Add(opciones[int.Parse(menu_opciones) - 1]);
-                        Console.WriteLine($"Saldo de cuenta: {saldoCuenta.ToString("f2")}");
-                        Console.ReadKey();
-                        break;
+            return retorno;
 
-                    case "3":
+        }
+
+        public static void opcionesPrincipales(string[] opciones, string[] datos, bool menu, double saldoCuenta){
+            List<string> Logs = new List<string>();
+            for (int i = 0; i < opciones.Length; i++){
+                Console.WriteLine($"{i + 1}.{opciones[i]}");
+            }
+            Console.Write("Seleccione la accion que desea ejecutar indicando el numero de opcion: ");
+            int menuOpciones = int.Parse(Console.ReadLine() ?? string.Empty);
+            switchCase2(menuOpciones, Logs, datos, menu, opciones, saldoCuenta);            
+        }
+    
+
+            public static string[] switchCase2( int menuOpciones, List<string> Logs, string[] datos, bool menu, string[] opciones, double saldoCuenta){
+            datos[5] = saldoCuenta.ToString();
+            var Opciones = new Opciones();
+               switch (menuOpciones){
+                    case 1:
+                        Opciones.infoCuenta(datos);
+                        Opciones.printLogs(Logs);
+                    break;
+                    
+                    case 2 :
+                        saldoCuenta = Opciones.compraProducto(saldoCuenta);
+                        Opciones.addLogs(Logs, opciones, menuOpciones);
+                    break;
+/* 
+
+                    case 3:
                         Console.Clear();
                         if (saldoCuenta >= 500){
                             saldoCuenta = saldoCuenta + (saldoCuenta * 0.11);
-                            Logs.Add(opciones[int.Parse(menu_opciones) - 1]);
+                            Logs.Add(opciones[menuOpciones - 1]);
                             Console.WriteLine($"Saldo de cuenta: {saldoCuenta.ToString("f2")}");
                         }
                         else{
@@ -60,7 +124,7 @@
                         Console.ReadKey();
                         break;
 
-                    case "4":
+                    case 4:
                         if (abonarCuenta >= 30){
                             contadorAbono = 0;
                             abonarCuenta = 0;
@@ -68,7 +132,7 @@
                         Console.Clear();
                         if (contadorAbono < 2 && saldoCuenta < 500){
                             saldoCuenta = saldoCuenta * 2;
-                            Logs.Add(opciones[int.Parse(menu_opciones) - 1]);
+                            Logs.Add(opciones[menuOpciones - 1]);
                             Console.WriteLine($"Saldo de cuenta: {saldoCuenta.ToString("f2")}");
                             contadorAbono++;
                         }
@@ -78,8 +142,8 @@
                         Console.ReadKey();
                         break;
 
-                    case "5":
-                        /* Interes simple = Capital * Porcentaje de interes * Tiempo (días) / 360  */
+                    case 5:
+
                         Console.Write("Ingrese el periodo de capitalizacion deseado en dias: ");
                         periodoCapitalizacion = int.Parse(Console.ReadLine() ?? string.Empty);
                         abonarCuenta += periodoCapitalizacion;
@@ -88,16 +152,16 @@
                             Thread.Sleep(1000);
                             Console.Clear();
                         }
-                        double intereses = saldoCuenta * 0.02 * periodoCapitalizacion / 360;
+                        float intereses = saldoCuenta * 0.02 * periodoCapitalizacion / 360;
                         saldoCuenta = saldoCuenta + intereses;
-                        Logs.Add(opciones[int.Parse(menu_opciones) - 1]);
+                        Logs.Add(opciones[menuOpciones - 1]);
                         Console.WriteLine($"Saldo de cuenta: {saldoCuenta.ToString("f2")}, genero: {intereses.ToString("f2")} en intereses.");
                         Console.ReadKey();
                         break;
 
-                    case "6":
+                    case 6:
                         menu = false;
-                        break;
+                        break; */
 
                     default:
                         Console.Clear();
@@ -106,49 +170,47 @@
                         break;
 
                 }
-
-            }
-        }
-        public static string[] TomarDatos(){
-            string[] datos = new string[6];
-            Console.WriteLine("1. Cuenta monetaria Quetzales.\n2.Cuenta monetaria Dolares\n3.Cuenta de ahorro en Quetzales.\n4.Cuenta de ahorro en Dolares.");
-            Console.Write("Seleccione su tipo de cuenta indicando el numero de la opcion deseada: ");
-            int tc = int.Parse(Console.ReadLine() ?? string.Empty);
-            switch (tc){
-                case 1:
-                    datos[0] = "Cuenta monetaria Quetzales";
-                    break;
-
-                case 2:
-                    datos[0] = "Cuenta monetaria Dolares";
-                    break;
-
-                case 3:
-                    datos[0] = "Cuenta de ahorro Quetzales";
-                    break;
-
-                case 4:
-                    datos[0] = "Cuenta de ahorro Dolares";
-                    break;
-
-                default:
-                    break;
-            }
-            Console.WriteLine("A continuacion ingrese los datos solicitados");
-            Console.Write("Ingrese su nombre: ");
-            datos[1] = Console.ReadLine() ?? string.Empty;
-            do{
-                Console.Write("Ingrese su numero de DPI (Campo estricto de 5 caracteres): ");
-                datos[2] = Console.ReadLine() ?? string.Empty;
-            } while (datos[2].Length < 5 || datos[2].Length > 5);
-            Console.Write("Ingrese su direccion de residencia: ");
-            datos[3] = Console.ReadLine() ?? string.Empty;
-            Console.Write("Ingrese su numero de telefono: ");
-            datos[4] = Console.ReadLine() ?? string.Empty;
-            datos[5] = "2500";
-            Console.WriteLine($"Su saldo inicial sera de {datos[5]}");
             return datos;
+
+        }
+    }
+
+    class Opciones{
+        /* "Ver informacion de la cuenta", "Compra de producto Financiero", "Venta de producto Financiero", "Abono a cuenta", "Simulacion paso del tiempo", "Salir" */
+        public void infoCuenta(string[] datos){
+            Console.Clear();
+            Console.WriteLine($"Tipo de cuenta: {datos[0]}\nNombre cuentaHabiente: {datos[1]}\nNo. de DPI: {datos[2]}\nDireccion: {datos[3]}\nNo. de Telefono: {datos[4]}\nSaldo de cuenta: ${datos[5]}");
+            
         }
 
+        public double compraProducto(double saldoCuenta){
+            saldoCuenta = saldoCuenta - (saldoCuenta * 0.1);
+            Console.WriteLine($"Saldo de cuenta: {saldoCuenta.ToString("f2")}");
+            Console.ReadKey();
+            return saldoCuenta;
+        }
+
+        public double ventaProducto(double saldoCuenta){
+            return saldoCuenta;
+        }
+
+        public double abonoCuenta(double saldoCuenta){
+            return saldoCuenta;
+        }
+
+        public double pasoTiempo(double saldoCuenta){
+            return saldoCuenta;
+        }
+
+        public static void printLogs(List<string> Logs){
+            Console.WriteLine("\n\nLogs: ");
+                        for (int i = 0; i < Logs.Count; i++){
+                            Console.WriteLine(Logs[i]);
+                        }
+        }
+
+        public static void addLogs(List<string> Logs,string[] opciones, int menuOpciones){
+                        Logs.Add(opciones[menuOpciones - 1]);
+        }
     }
 }
